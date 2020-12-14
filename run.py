7b6a3ff61c49bbcc35f278c9b8b8028ef1ad0a68
Rtypus <42766231+Rtypus@users.noreply.github.com>
2020-12-14 19:42:57 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,533 @@
+#from dqn_agent import DQNAgent
+from tetris import Tetris
+from datetime import datetime
+from statistics import mean, median
+import random
+import cv2
+from PIL import Image
+from time import sleep
+
+#from logs import CustomTensorBoard
+from tqdm import tqdm
+import tensorflow as tf
+from tensorflow import keras
+
+from keras.models import Sequential, save_model, load_model
+from keras.layers import Dense
+from collections import deque
+import numpy as np
+
+log_dir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir)
+
+class Tetris:
+
+    '''Tetris game class'''
+
+    # BOARD
+    MAP_EMPTY = 0
+    MAP_BLOCK = 1
+    MAP_PLAYER = 2
+    BOARD_WIDTH = 10
+    BOARD_HEIGHT = 20
+
+    TETROMINOS = {
+        0: { # I
+            0: [(0,0), (1,0), (2,0), (3,0)],
+            90: [(1,0), (1,1), (1,2), (1,3)],
+            180: [(3,0), (2,0), (1,0), (0,0)],
+            270: [(1,3), (1,2), (1,1), (1,0)],
+        },
+        1: { # T
+            0: [(1,0), (0,1), (1,1), (2,1)],
+            90: [(0,1), (1,2), (1,1), (1,0)],
+            180: [(1,2), (2,1), (1,1), (0,1)],
+            270: [(2,1), (1,0), (1,1), (1,2)],
+        },
+        2: { # L
+            0: [(1,0), (1,1), (1,2), (2,2)],
+            90: [(0,1), (1,1), (2,1), (2,0)],
+            180: [(1,2), (1,1), (1,0), (0,0)],
+            270: [(2,1), (1,1), (0,1), (0,2)],
+        },
+        3: { # J
+            0: [(1,0), (1,1), (1,2), (0,2)],
+            90: [(0,1), (1,1), (2,1), (2,2)],
+            180: [(1,2), (1,1), (1,0), (2,0)],
+            270: [(2,1), (1,1), (0,1), (0,0)],
+        },
+        4: { # Z
+            0: [(0,0), (1,0), (1,1), (2,1)],
+            90: [(0,2), (0,1), (1,1), (1,0)],
+            180: [(2,1), (1,1), (1,0), (0,0)],
+            270: [(1,0), (1,1), (0,1), (0,2)],
+        },
+        5: { # S
+            0: [(2,0), (1,0), (1,1), (0,1)],
+            90: [(0,0), (0,1), (1,1), (1,2)],
+            180: [(0,1), (1,1), (1,0), (2,0)],
+            270: [(1,2), (1,1), (0,1), (0,0)],
+        },
+        6: { # O
+            0: [(1,0), (2,0), (1,1), (2,1)],
+            90: [(1,0), (2,0), (1,1), (2,1)],
+            180: [(1,0), (2,0), (1,1), (2,1)],
+            270: [(1,0), (2,0), (1,1), (2,1)],
+        }
+    }
+
+    COLORS = {
+        0: (255, 255, 255),
+        1: (247, 64, 99),
+        2: (0, 167, 247),
+    }
+
+
+    def __init__(self):
+        self.reset()
+
+    
+    def reset(self):
+        '''Resets the game, returning the current state'''
+        self.board = [[0] * Tetris.BOARD_WIDTH for _ in range(Tetris.BOARD_HEIGHT)]
+        self.game_over = False
+        self.bag = list(range(len(Tetris.TETROMINOS)))
+        random.shuffle(self.bag)
+        self.next_piece = self.bag.pop()
+        self._new_round()
+        self.score = 0
+        return self._get_board_props(self.board)
+
+
+    def _get_rotated_piece(self):
+        '''Returns the current piece, including rotation'''
+        return Tetris.TETROMINOS[self.current_piece][self.current_rotation]
+
+
+    def _get_complete_board(self):
+        '''Returns the complete board, including the current piece'''
+        piece = self._get_rotated_piece()
+        piece = [np.add(x, self.current_pos) for x in piece]
+        board = [x[:] for x in self.board]
+        for x, y in piece:
+            board[y][x] = Tetris.MAP_PLAYER
+        return board
+
+
+    def get_game_score(self):
+        '''Returns the current game score.
+        Each block placed counts as one.
+        For lines cleared, it is used BOARD_WIDTH * lines_cleared ^ 2.
+        '''
+        return self.score
+    
+
+    def _new_round(self):
+        '''Starts a new round (new piece)'''
+        # Generate new bag with the pieces
+        if len(self.bag) == 0:
+            self.bag = list(range(len(Tetris.TETROMINOS)))
+            random.shuffle(self.bag)
+        
+        self.current_piece = self.next_piece
+        self.next_piece = self.bag.pop()
+        self.current_pos = [3, 0]
+        self.current_rotation = 0
+
+        if self._check_collision(self._get_rotated_piece(), self.current_pos):
+            self.game_over = True
+
+
+    def _check_collision(self, piece, pos):
+        '''Check if there is a collision between the current piece and the board'''
+        for x, y in piece:
+            x += pos[0]
+            y += pos[1]
+            if x < 0 or x >= Tetris.BOARD_WIDTH \
+                    or y < 0 or y >= Tetris.BOARD_HEIGHT \
+                    or self.board[y][x] == Tetris.MAP_BLOCK:
+                return True
+        return False
+
+
+    def _rotate(self, angle):
+        '''Change the current rotation'''
+        r = self.current_rotation + angle
+
+        if r == 360:
+            r = 0
+        if r < 0:
+            r += 360
+        elif r > 360:
+            r -= 360
+
+        self.current_rotation = r
+
+
+    def _add_piece_to_board(self, piece, pos):
+        '''Place a piece in the board, returning the resulting board'''        
+        board = [x[:] for x in self.board]
+        for x, y in piece:
+            board[y + pos[1]][x + pos[0]] = Tetris.MAP_BLOCK
+        return board
+
+
+    def _clear_lines(self, board):
+        '''Clears completed lines in a board'''
+        # Check if lines can be cleared
+        lines_to_clear = [index for index, row in enumerate(board) if sum(row) == Tetris.BOARD_WIDTH]
+        if lines_to_clear:
+            board = [row for index, row in enumerate(board) if index not in lines_to_clear]
+            # Add new lines at the top
+            for _ in lines_to_clear:
+                board.insert(0, [0 for _ in range(Tetris.BOARD_WIDTH)])
+        return len(lines_to_clear), board
+
+
+    def _number_of_holes(self, board):
+        '''Number of holes in the board (empty sqquare with at least one block above it)'''
+        holes = 0
+
+        for col in zip(*board):
+            i = 0
+            while i < Tetris.BOARD_HEIGHT and col[i] != Tetris.MAP_BLOCK:
+                i += 1
+            holes += len([x for x in col[i+1:] if x == Tetris.MAP_EMPTY])
+
+        return holes
+
+
+    def _bumpiness(self, board):
+        '''Sum of the differences of heights between pair of columns'''
+        total_bumpiness = 0
+        max_bumpiness = 0
+        min_ys = []
+
+        for col in zip(*board):
+            i = 0
+            while i < Tetris.BOARD_HEIGHT and col[i] != Tetris.MAP_BLOCK:
+                i += 1
+            min_ys.append(i)
+        
+        for i in range(len(min_ys) - 1):
+            bumpiness = abs(min_ys[i] - min_ys[i+1])
+            max_bumpiness = max(bumpiness, max_bumpiness)
+            total_bumpiness += abs(min_ys[i] - min_ys[i+1])
+
+        return total_bumpiness, max_bumpiness
+
+
+    def _height(self, board):
+        '''Sum and maximum height of the board'''
+        sum_height = 0
+        max_height = 0
+        min_height = Tetris.BOARD_HEIGHT
+
+        for col in zip(*board):
+            i = 0
+            while i < Tetris.BOARD_HEIGHT and col[i] == Tetris.MAP_EMPTY:
+                i += 1
+            height = Tetris.BOARD_HEIGHT - i
+            sum_height += height
+            if height > max_height:
+                max_height = height
+            elif height < min_height:
+                min_height = height
+
+        return sum_height, max_height, min_height
+
+
+    def _get_board_props(self, board):
+        '''Get properties of the board'''
+        lines, board = self._clear_lines(board)
+        holes = self._number_of_holes(board)
+        total_bumpiness, max_bumpiness = self._bumpiness(board)
+        sum_height, max_height, min_height = self._height(board)
+        return [lines, holes, total_bumpiness, sum_height]
+
+
+    def get_next_states(self):
+        '''Get all possible next states'''
+        states = {}
+        piece_id = self.current_piece
+        
+        if piece_id == 6: 
+            rotations = [0]
+        elif piece_id == 0:
+            rotations = [0, 90]
+        else:
+            rotations = [0, 90, 180, 270]
+
+        # For all rotations
+        for rotation in rotations:
+            piece = Tetris.TETROMINOS[piece_id][rotation]
+            min_x = min([p[0] for p in piece])
+            max_x = max([p[0] for p in piece])
+
+            # For all positions
+            for x in range(-min_x, Tetris.BOARD_WIDTH - max_x):
+                pos = [x, 0]
+
+                # Drop piece
+                while not self._check_collision(piece, pos):
+                    pos[1] += 1
+                pos[1] -= 1
+
+                # Valid move
+                if pos[1] >= 0:
+                    board = self._add_piece_to_board(piece, pos)
+                    states[(x, rotation)] = self._get_board_props(board)
+
+        return states
+
+
+    def get_state_size(self):
+        '''Size of the state'''
+        return 4
+
+
+    def play(self, x, rotation, render=False, render_delay=None):
+        '''Makes a play given a position and a rotation, returning the reward and if the game is over'''
+        self.current_pos = [x, 0]
+        self.current_rotation = rotation
+
+        # Drop piece
+        while not self._check_collision(self._get_rotated_piece(), self.current_pos):
+            if render:
+                self.render()
+                if render_delay:
+                    sleep(render_delay)
+            self.current_pos[1] += 1
+        self.current_pos[1] -= 1
+
+        # Update board and calculate score        
+        self.board = self._add_piece_to_board(self._get_rotated_piece(), self.current_pos)
+        lines_cleared, self.board = self._clear_lines(self.board)
+        score = 1 + (lines_cleared ** 2) * Tetris.BOARD_WIDTH
+        self.score += score
+
+        # Start new round
+        self._new_round()
+        if self.game_over:
+            score -= 2
+
+        return score, self.game_over
+
+
+    def render(self):
+        '''Renders the current board'''
+        img = [Tetris.COLORS[p] for row in self._get_complete_board() for p in row]
+        img = np.array(img).reshape(Tetris.BOARD_HEIGHT, Tetris.BOARD_WIDTH, 3).astype(np.uint8)
+        img = img[..., ::-1] # Convert RRG to BGR (used by cv2)
+        img = Image.fromarray(img, 'RGB')
+        img = img.resize((Tetris.BOARD_WIDTH * 25, Tetris.BOARD_HEIGHT * 25))
+        img = np.array(img)
+        cv2.putText(img, str(self.score), (22, 22), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+        cv2.imshow('image', np.array(img))
+        cv2.waitKey(1)
+
+
+class DQNAgent:
+
+
+    def __init__(self, state_size, mem_size=10000, discount=0.95,
+                 epsilon=1, epsilon_min=0, epsilon_stop_episode=500,
+                 n_neurons=[32,32], activations=['relu', 'relu', 'linear'],
+                 loss='mse', optimizer='adam', replay_start_size=None):
+
+        assert len(activations) == len(n_neurons) + 1
+
+        self.state_size = state_size
+        self.memory = deque(maxlen=mem_size)
+        self.discount = discount
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = (self.epsilon - self.epsilon_min) / (epsilon_stop_episode)
+        self.n_neurons = n_neurons
+        self.activations = activations
+        self.loss = loss
+        self.optimizer = optimizer
+        if not replay_start_size:
+            replay_start_size = mem_size / 2
+        self.replay_start_size = replay_start_size
+        self.model = self._build_model()
+
+
+    def _build_model(self):
+        '''Builds a Keras deep neural network model'''
+        model = Sequential()
+        model.add(Dense(self.n_neurons[0], input_dim=self.state_size, activation=self.activations[0]))
+
+        model.compile(loss='mse', optimizer=keras.optimizers.SGD(lr=0.2),)
+
+
+        for i in range(1, len(self.n_neurons)):
+            model.add(Dense(self.n_neurons[i], activation=self.activations[i]))
+
+        model.add(Dense(1, activation=self.activations[-1]))
+
+        model.compile(loss=self.loss, optimizer=self.optimizer)
+        
+        return model
+
+
+    def add_to_memory(self, current_state, next_state, reward, done):
+        '''Adds a play to the replay memory buffer'''
+        self.memory.append((current_state, next_state, reward, done))
+
+
+    def random_value(self):
+        '''Random score for a certain action'''
+        return random.random()
+
+
+    def predict_value(self, state):
+        '''Predicts the score for a certain state'''
+        return self.model.predict(state)[0]
+
+
+    def act(self, state):
+        '''Returns the expected score of a certain state'''
+        state = np.reshape(state, [1, self.state_size])
+        if random.random() <= self.epsilon:
+            return self.random_value()
+        else:
+            return self.predict_value(state)
+
+
+    def best_state(self, states):
+        '''Returns the best state for a given collection of states'''
+        max_value = None
+        best_state = None
+
+        if random.random() <= self.epsilon:
+            return random.choice(list(states))
+
+        else:
+            for state in states:
+                value = self.predict_value(np.reshape(state, [1, self.state_size]))
+                if not max_value or value > max_value:
+                    max_value = value
+                    best_state = state
+
+        return best_state
+
+
+    def train(self, batch_size=32, epochs=3):
+        '''Trains the agent'''
+        n = len(self.memory)
+    
+        if n >= self.replay_start_size and n >= batch_size:
+
+            batch = random.sample(self.memory, batch_size)
+
+            # Get the expected score for the next states, in batch (better performance)
+            next_states = np.array([x[1] for x in batch])
+            next_qs = [x[0] for x in self.model.predict(next_states)]
+
+            x = []
+            y = []
+
+            # Build xy structure to fit the model in batch (better performance)
+            for i, (state, _, reward, done) in enumerate(batch):
+                if not done:
+                    # Partial Q formula
+                    new_q = reward + self.discount * next_qs[i]
+                else:
+                    new_q = reward
+
+                x.append(state)
+                y.append(new_q)
+            #log_dir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+            #log = CustomTensorBoard(log_dir=log_dir)
+            #tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+            # Fit the model to the given values
+            #self.model.fit(np.array(x), np.array(y), batch_size=batch_size, epochs=epochs,callbacks=dqn.callbacks)
+            self.model.fit(np.array(x), np.array(y), batch_size=batch_size, epochs=epochs,callbacks=[tensorboard_callback])
+
+            # Update the exploration variable
+            if self.epsilon > self.epsilon_min:
+                self.epsilon -= self.epsilon_decay
+# Run dqn with Tetris
+def dqn():
+    env = Tetris()
+    episodes = 2000
+    max_steps = None
+    epsilon_stop_episode = 1500
+    mem_size = 20000
+    discount = 0.95
+    batch_size = 512
+    epochs = 1
+    render_every = 50
+    log_every = 50
+    replay_start_size = 2000
+    train_every = 1
+    n_neurons = [32, 32]
+    render_delay = None
+    activations = ['relu', 'relu', 'linear']
+    
+
+    agent = DQNAgent(env.get_state_size(),
+                     n_neurons=n_neurons, activations=activations,
+                     epsilon_stop_episode=epsilon_stop_episode, mem_size=mem_size,
+                     discount=discount, replay_start_size=replay_start_size)
+
+    #log_dir = f'logs/tetris-nn={str(n_neurons)}-mem={mem_size}-bs={batch_size}-e={epochs}-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+    #log = CustomTensorBoard(log_dir=log_dir)
+    #tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+    #log_dir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    #tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+    #logdir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    #tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir)
+
+
+
+
+    scores = []
+
+    for episode in tqdm(range(episodes)):
+        current_state = env.reset()
+        done = False
+        steps = 0
+
+        if render_every and episode % render_every == 0:
+            render = True
+        else:
+            render = False
+
+        # Game
+        while not done and (not max_steps or steps < max_steps):
+            next_states = env.get_next_states()
+            best_state = agent.best_state(next_states.values())
+            
+            best_action = None
+            for action, state in next_states.items():
+                if state == best_state:
+                    best_action = action
+                    break
+
+            reward, done = env.play(best_action[0], best_action[1], render=render,
+                                    render_delay=render_delay)
+            
+            agent.add_to_memory(current_state, next_states[best_action], reward, done)
+            current_state = next_states[best_action]
+            steps += 1
+
+        scores.append(env.get_game_score())
+
+        # Train
+        if episode % train_every == 0:
+            agent.train(batch_size=batch_size, epochs=epochs)
+
+        # Logs
+        if log_every and episode and episode % log_every == 0:
+            avg_score = mean(scores[-log_every:])
+            min_score = min(scores[-log_every:])
+            max_score = max(scores[-log_every:])
+            
+            #log.log(episode, avg_score=avg_score, min_score=min_score,
+                    #max_score=max_score)
+    print(log_dir)
+
+if __name__ == "__main__":
+    dqn()
